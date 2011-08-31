7cbe136a7140c37c01457263a50ada4dd7d784b4
Renaud Allard <renaud@allard.it>
2011-08-31 11:48:03 +0200

    Update

--- getgrsec.sh
+++ getgrsec.sh
@@ -6,8 +6,11 @@ do
 	ftp -C -V "https://grsecurity.net/test/${URL}"
 done
 
+find grsecurity-* -ctime +180 -exec rm {} \;
+
+for i in `git status | grep deleted | awk '{print $2}'`; do git update-index --assume-unchanged $i; done
+
 git add *
 git commit -a -C 6a0abdba5dd74430d661317c6cf7458a04a9348f
 git push
-
 
