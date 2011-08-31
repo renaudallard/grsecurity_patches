343a01ec43f8632d17e5470458d30c2ebfd4ff7b
Renaud Allard <renaud@allard.it>
2011-08-31 11:48:03 +0200

    Update

--- getgrsec.sh
+++ getgrsec.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 wget -q "http://grsecurity.net/test.php" -O grsec.html
 grep grsecurity- grsec.html > grsec2.html
-perl -p -i -e 's/href=/\n/g' /tmp/grsec2.html 
+perl -p -i -e 's/href=/\n/g' grsec2.html 
 cat grsec2.html |  awk '{print $1}' |egrep -v "\.sig|div" | sed 's/"//g' | while read URL
 do
+	echo ${URL}
 	wget -c -q "http://grsecurity.net/${URL}"
 done
 
