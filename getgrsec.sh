31f973b17bf355c16581d594207f0a276f9862e8
Renaud Allard <renaud@allard.it>
2011-08-31 11:48:03 +0200

    Update

--- getgrsec.sh
+++ getgrsec.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
-wget -q "http://grsecurity.net/test.php" -O /tmp/grsec.html
-grep grsecurity- /tmp/grsec.html > /tmp/grsec2.html
+wget -q "http://grsecurity.net/test.php" -O grsec.html
+grep grsecurity- grsec.html > grsec2.html
 perl -p -i -e 's/href=/\n/g' /tmp/grsec2.html 
-cat /tmp/grsec2.html |  awk '{print $1}' |egrep -v "\.sig|div" | sed 's/"//g' | while read URL
+cat grsec2.html |  awk '{print $1}' |egrep -v "\.sig|div" | sed 's/"//g' | while read URL
 do
-	wget -q "http://grsecurity.net/${URL}"
+	wget -c -q "http://grsecurity.net/${URL}"
 done
+
+rm grsec.html grsec2.html 
+
 git add *
 git commit -a -C 6a0abdba5dd74430d661317c6cf7458a04a9348f
 git push
+
+
